26bd1919396c510243de2f457628fd9657596e93
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-09 11:48:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,9 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index()
-
-
+def input_to_index(user_input)
+  integer = user_input.to_i-1
+  return integer
 end
 
 
